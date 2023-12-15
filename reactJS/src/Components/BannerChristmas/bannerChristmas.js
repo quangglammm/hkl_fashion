f6f03a8a4ef664066ae4ffe73e865ab34344a1d7
hau1605 <phmh165@gmail.com>
2023-12-15 22:47:55 +0700

    update login

--- reactJS/src/Components/BannerChristmas/bannerChristmas.js
+++ reactJS/src/Components/BannerChristmas/bannerChristmas.js
@@ -1,5 +1,7 @@
 import banner1 from "../Images/banner/bannerChristmas.png";
+import banner2 from "../Images/banner/bannerChristmas2.png";
 
 export const sliderData=[
-  {image: banner1}
+  {image: banner1},
+  {image: banner2}
 ]

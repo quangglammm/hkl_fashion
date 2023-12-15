9319a64b163d09a66f0a739376bafc939b1224af
minghunt <114232628+minghunt@users.noreply.github.com>
2023-12-15 23:05:26 +0700

    add css header footer

--- reactJS/src/Components/LayoutChristmas.js
+++ reactJS/src/Components/LayoutChristmas.js
@@ -1,9 +1,10 @@
 import React, { Suspense} from 'react';
 import HeaderChristmas from './HeaderDefault/HeaderChristmas.jsx'
 import FooterChristmas from './FooterChristmas/FooterChristmas.jsx'
-
+import Snowflakes from '../cssEffect/Snowflakes.js';
 const LayoutChristmas = ({ children }) => {
     return(<>
+        <Snowflakes/>
         <HeaderChristmas />
 
         <Suspense fallback={<p>Loading...</p>}>
--- reactJS/src/index.js
+++ reactJS/src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import Layout from "./Components/Layout";
-// import Layout from "./Components/LayoutChristmas";
+import LayoutChristmas from "./Components/LayoutChristmas";
 import LayoutAdmin from "./Components/LayoutAdmin";
 
 import Banner from "./Components/Banner/Banner.jsx";
@@ -66,11 +66,11 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: (
-                    <Layout>
-                        <Banner />
-                        {/* <BannerChristmas /> */}
+                    <LayoutChristmas>
+                        {/* <Banner /> */}
+                        <BannerChristmas />
                         <MainPage />
-                    </Layout>
+                    </LayoutChristmas>
                 ),
             },
             {

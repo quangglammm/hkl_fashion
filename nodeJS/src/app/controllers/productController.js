2e949338de53dba83998a7ce007ea11cf2be3c63
quangglammm <20521514@gm.uit.edu.vn>
2024-01-02 21:57:09 +0700

    caching aside for products using redis

--- nodeJS/src/app/controllers/productController.js
+++ nodeJS/src/app/controllers/productController.js
@@ -3,46 +3,99 @@ const ProductDetail = require('../models/products/product_detail');
 const CategoryDetail = require('../models/products/category_detail');
 const ProductComment = require('../models/products/product_comment');
 const { HandleAddImage } = require('../../helpers/multifunction');
-class ProductController {
-    // GET /product/discount
-    Discount(req, res, next) {
-        Product.find({ discount: { $ne: 0, $ne: null } })
+const client = require('../../helpers/connection_redis');
+
+// Function to get data from cache or database
+function getDataFromCacheOrDatabase(cacheKey, databaseQuery, res, next) {
+    // Check if data is in Redis cache
+    client.get(cacheKey, (err, cachedData) => {
+        if (err) {
+            console.error('Error retrieving data from Redis cache:', err);
+            return next(err);
+        }
+
+        if (cachedData) {
+            // If cached data exists, send it as the response
+            console.log('Data retrieved from Redis cache');
+            const parsedData = JSON.parse(cachedData);
+            return res.json(parsedData);
+        }
+
+        // If not in cache, query the database
+        databaseQuery()
             .exec()
             .then((products) => {
                 const data = products.map(HandleAddImage);
+
+                // Use Promise.all to wait for all asynchronous operations to complete
                 Promise.all(data).then((result) => {
-                    res.json(result);
+                    // Send the response
+                    res.json(result); // Sending the result, not the original 'data'
+                    console.log('Data cached in Redis');
+
+                    // Save result to Redis cache
+                    client.setex(cacheKey, 300, JSON.stringify(result)); // Set expiration time to 1 minute
                 });
             })
             .catch(next);
+    });
+}
+class ProductController {
+    // GET /product/discount
+    Discount(req, res, next) {
+        // Product.find({ discount: { $ne: 0, $ne: null } })
+        //     .exec()
+        //     .then((products) => {
+        //         const data = products.map(HandleAddImage);
+        //         Promise.all(data).then((result) => {
+        //             res.json(result);
+        //         });
+        //     })
+        //     .catch(next);
+
+        getDataFromCacheOrDatabase(
+            'discountedProducts',
+            () => Product.find({ discount: { $ne: 0, $ne: null } }),
+            res,
+            next,
+        );
     }
 
     // GET /product/top-selling
     TopSelling(req, res, next) {
-        Product.aggregate([{ $sample: { size: 10 } }])
-            .exec()
-            .then((products) => {
-                const data = products.map(HandleAddImage);
-                Promise.all(data).then((result) => {
-                    res.json(result);
-                });
-            })
-            .catch(next);
+        // Product.aggregate([{ $sample: { size: 10 } }])
+        //     .exec()
+        //     .then((products) => {
+        //         const data = products.map(HandleAddImage);
+        //         Promise.all(data).then((result) => {
+        //             res.json(result);
+        //         });
+        //     })
+        //     .catch(next);
+
+        getDataFromCacheOrDatabase(
+            'topSellingProducts',
+            () => Product.aggregate([{ $sample: { size: 10 } }]),
+            res,
+            next,
+        );
     }
 
     // GET /product/new
     New(req, res, next) {
-        Product.find({})
-            .sort({ createdAt: -1 })
-            .limit(10)
-            .exec()
-            .then((products) => {
-                const data = products.map(HandleAddImage);
-                Promise.all(data).then((result) => {
-                    res.json(result);
-                });
-            })
-            .catch(next);
+        // Product.find({})
+        //     .sort({ createdAt: -1 })
+        //     .limit(10)
+        //     .exec()
+        //     .then((products) => {
+        //         const data = products.map(HandleAddImage);
+        //         Promise.all(data).then((result) => {
+        //             res.json(result);
+        //         });
+        //     })
+        //     .catch(next);
+
+        getDataFromCacheOrDatabase('newProducts', () => Product.find({}).sort({ createdAt: -1 }).limit(10), res, next);
     }
 
     // GET /product/:id
@@ -54,6 +107,7 @@ class ProductController {
                 Promise.all(data).then((result) => res.json(result));
             })
             .catch(next);
+
     }
 
     // GET /product/:id/product-detail?color=:slug
@@ -159,7 +213,7 @@ class ProductController {
             .exec()
             .then((productComments) => res.json(productComments));
     }
-    
+
     // GET /product/:id/count-comment
     CountComment(req, res, next) {
         //     ProductComment.countDocuments({ product_id: req.params.id }).then((qty) => {
@@ -187,7 +241,7 @@ class ProductController {
                 for (let i = 0; i < qtyCmt; i++) {
                     totalStar += comments[i].star;
                 }
-                const avgStar = (totalStar / qtyCmt).toFixed(2);;
+                const avgStar = (totalStar / qtyCmt).toFixed(2);
                 res.json({
                     avgStar: parseFloat(avgStar),
                     qtyCmt,
